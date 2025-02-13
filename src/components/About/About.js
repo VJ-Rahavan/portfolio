5e2e9fbf86c979b4b82173cb15b9e206ecf5ff67
Vijayarahavan <vijayarahavan176@gmail.com>
2025-02-13 23:19:36 +0530

    feat: updated resume

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ import { SectionHeading, SectionTitle } from '../styles/SectionHeading';
 import { StyledParagraph } from '../styles/Typography.styled';
 import myImage from '../../assets/my-name.png';
 import downloadIcon from '../../assets/icons/download-icon.svg';
-import resume from '../../assets/Resume.pdf';
+import resume from '../../assets/resume.pdf';
 
 const About = () => {
   return (
